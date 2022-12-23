6c1a7871b780d47703853414cdfaeb20f6809ffe
WhiteLight <qeros@yandex.ru>
2022-12-23 18:14:09 +0300

    java_core-lesson2

--- JavaCoreProject/src/main/java/ru/geekbrains/core/lesson2/Main.java
+++ JavaCoreProject/src/main/java/ru/geekbrains/core/lesson2/Main.java
@@ -6,8 +6,8 @@ import ru.geekbrains.core.lesson2.MyException.MyArrayDataException;
 public class Main {
 
     static String[][] arraySizeException = {{"1", "2"}, {"1", "6",}, {"9", "6"}, {"5", "6"}};
-    static String[][] arrayDataException = {{"1", "2", "3", "4"}, {"7", "6", "7", "8"}, {"9", "ш", "7", "8"}, {"5", "6", "7", "8"}};
-    static String[][] arrayWithNoError = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "6", "7", "8"}, {"5", "6", "7", "8"}};
+    static String[][] arrayDataException = {{"1", "%", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}};
+    static String[][] arrayWithNoError = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"8", "7", "6", "5"}, {"4", "3", "2", "1"}};
 
     static int sizeA = 4;
     static int sizeB = 4;
@@ -38,7 +38,7 @@ public class Main {
     }
 
     static void checkValue(String[][] array) throws MyArraySizeException {
-        if(array.length!=sizeA || array[0].length!=sizeB) throw new MyArraySizeException();
+        if(array.length != sizeA || array[0].length != sizeB) throw new MyArraySizeException();
     }
 
     static Integer getSumm(String[][] array) throws MyArrayDataException,MyArraySizeException {
@@ -47,9 +47,8 @@ public class Main {
         int i = 0;
         int j = 0;
         try {
-            for (; i < array.length; i++) {
-                j = 0;
-                for (; j < array.length; j++) {
+            for (i = 0; i < array.length; i++) {
+                for (j = 0; j < array.length; j++) {
                     result = result + Integer.parseInt(array[i][j]);
                 }
             }
