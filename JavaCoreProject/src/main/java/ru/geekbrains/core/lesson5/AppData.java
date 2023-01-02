dcd1bddac5a8cd13949c84939cf09f2df4e5aaeb
WhiteLight <qeros@yandex.ru>
2023-01-02 20:31:46 +0300

    java_core-lesson5

---
+++ JavaCoreProject/src/main/java/ru/geekbrains/core/lesson5/AppData.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.core.lesson5;
+
+import java.io.*;
+
+public class AppData {
+
+    private String[] header;
+    private int[][] data;
+
+    public void setHeader(String[] header) {
+        this.header = header;
+    }
+
+    public void setData(int[][] data) {
+        this.data = data;
+    }
+
+}
