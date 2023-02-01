796de7477b4705ef8f1e75dbc9534c08b80f39bc
WhiteLight <qeros@yandex.ru>
2023-02-01 23:45:33 +0300

    java_core-lesson9

---
+++ JavaCoreProject/src/main/java/ru/geekbrains/core/lesson9/Course.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.core.lesson9;
+
+import java.util.Objects;
+
+public class Course {
+
+    private String name;
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(name, course.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
--- JavaCoreProject/src/main/java/ru/geekbrains/core/lesson9/Main.java
+++ JavaCoreProject/src/main/java/ru/geekbrains/core/lesson9/Main.java
@@ -1,7 +1,56 @@
 package ru.geekbrains.core.lesson9;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Main {
+
     public static void main(String[] args) {
+        List<Student> result = getListOfStudents(3,3);
+        //1 задание
+        List<Course> uniqueCourses = result.stream().flatMap(student -> student.getCourseList().stream()).distinct().collect(Collectors.toList());
+        //result.stream().flatMap(student -> student.getCourseList().stream()).distinct().collect(Collectors.toList());
+        result.stream().flatMap(student -> student.getCourseList().stream()).collect(Collectors.toSet());
+
+        for (Course course : uniqueCourses) {
+            System.out.println(String.valueOf(course.getName()));
+        }
+
+        System.out.println();
+
+        //2 задание
+        List<Student> outputStudentsMaxCourses = result.stream().sorted((student1,student2) -> student1.getCourseList().size() - student2.getCourseList().size()).limit(3).collect(Collectors.toList());
+
+        for (Student student : outputStudentsMaxCourses) {
+            System.out.println(String.valueOf(student.getName()));
+        }
+
+        System.out.println();
+
+
+        //3 задание
+        Course paramCourse = new Course("course1");
+
+        List<Student> outputStudentsForCourse = result.stream().filter(student -> student.getCourseList().contains(paramCourse)).collect(Collectors.toList());
+
+        for (Student student : outputStudentsForCourse) {
+            System.out.println(String.valueOf(student.getName()));
+        }
 
     }
+
+    private static List<Student> getListOfStudents(int j, int m) {
+        ArrayList<Student> result = new ArrayList<>();
+        for (int i=0; i<j ;i++){
+            ArrayList<Course> courses = new ArrayList<>();
+            for (int h=0; h<m ;h++){
+                courses.add(new Course("course"+h));
+            }
+            result.add(new Student("student"+i, courses));
+        }
+        return result;
+    }
 }
+
